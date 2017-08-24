85082af30661f199bec3261f5ce0c86629d7ce41
Dru Jensen <drujensen@gmail.com>
2017-08-23 20:08:02 -0700

    minor cleanup

--- src/controllers/session_controller.cr
+++ src/controllers/session_controller.cr
@@ -4,17 +4,15 @@ class SessionController < Kemalyst::Controller
   end
 
   def create
-    email = params["email"]
-    password = params["password"]
-    user = User.find_by :email,  email
-    if user && user.authenticate(password)
-        session["user_id"] = user.id.to_s
-        flash["info"] = "Successfully logged in"
-        redirect "/"
-      else
-        flash["danger"] = "Invalid email or password"
-        html render("session/new.slang", "main.slang")
-      end
+    user = User.find_by :email,  params["email"]
+    if user && user.authenticate(params["password"])
+      session["user_id"] = user.id.to_s
+      flash["info"] = "Successfully logged in"
+      redirect "/"
+    else
+      flash["danger"] = "Invalid email or password"
+      html render("session/new.slang", "main.slang")
+    end
   end
 
   def delete
